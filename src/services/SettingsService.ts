df89ece6ccf3c8ef6931bad363fd7c81b7ec9b7d
Laura de Oliveira <l4ur4.88@gmail.com>
2021-04-25 00:32:42 -0300

    Aula 4 - Estruturando as connections

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -10,14 +10,14 @@ interface ISettingsCreate {
 class SettingsService {
   private settingsRepository: Repository<Setting>;
 
-  constructor(){
+  constructor() {
     this.settingsRepository = getCustomRepository(SettingsRepository);
   }
 
   async create({ chat, username }: ISettingsCreate) {
     const userAlreadyExists = await this.settingsRepository.findOne({ username });
 
-    if(userAlreadyExists)  {
+    if (userAlreadyExists) {
       throw new Error("User already exists!");
     }
 
@@ -29,6 +29,24 @@ class SettingsService {
     await this.settingsRepository.save(settings);
 
     return settings;
+  }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username
+    });
+
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    const settings = await this.settingsRepository.createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", {
+        username
+      })
+      .execute();
   }
 }
 
